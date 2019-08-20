bca1b075fda8718f4566ea79f3509baad7699642
mario salvatierra <msalv006@fiu.edu>
2019-08-19 21:21:57 -0400

    change random color for the view

--- AlphaApp/ViewController.swift
+++ AlphaApp/ViewController.swift
@@ -12,22 +12,40 @@ class ViewController: UIViewController {
 
     var colourIsGreen = false
     
+    @IBOutlet weak var labelver: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+       
         // Do any additional setup after loading the view.
     }
 
     @IBAction func mybutton(_ sender: Any) {
-        if colourIsGreen {
-            view.backgroundColor = UIColor.purple
-            colourIsGreen = false
-        }
-        else {
-            view.backgroundColor = UIColor.green
-            colourIsGreen = true
-        }
+    
         
+        let red = CGFloat.random(in: 0...1)
+        let green = CGFloat.random(in: 0...1)
+        let blue = CGFloat.random(in: 0...1)
+        let alpha = CGFloat.random(in: 0...1)
+        
+        let randomColor: UIColor = UIColor(red:red, green: green, blue: blue, alpha: alpha)
+      
+//        if colourIsGreen {
+//            view.backgroundColor = UIColor.purple
+//            colourIsGreen = false
+//        }
+//        else {
+//            view.backgroundColor = UIColor.green
+//            colourIsGreen = true
+//        }
+        let redstring = String(format: "%.2f", Double(red))
+        let greenstring = String(format: "%.2f", Double(green))
+        let bluestring = String(format: "%.2f", Double(blue))
+        let alphastring = String(format: "%.2f", Double(alpha))
+      view.backgroundColor = randomColor
+        let valueRGBA = "red: \(redstring)  green: \(greenstring)  blue:  \(bluestring) alpha: \(alphastring)"
+        
+        labelver.text = valueRGBA
     }
     
 }
